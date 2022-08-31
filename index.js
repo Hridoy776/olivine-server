99577b023b9c38cd1015c1498eef5f34a8a0ff47
Hridoy <rakibhasanhridoy776@gmail.com>
2022-08-31 18:10:14 +0600

    order delete by id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -47,6 +47,13 @@ async function run(){
 
             res.send({data:order,status:"success"});
 
+        })
+        // order delete api 
+        app.delete('/order/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id: ObjectId(id)};
+            const result= await orderCollection.deleteOne(query);
+            res.send(result);
         })
 
     }finally{
