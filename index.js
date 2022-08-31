55c8da29bb1d2327a085eee06f3a3faf42314318
Hridoy <rakibhasanhridoy776@gmail.com>
2022-08-31 18:06:39 +0600

    order get and post api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,11 +24,29 @@ async function run(){
         await client.connect()
 
         const foodCollection = client.db("olivine").collection("foods");
+        const orderCollection = client.db("olivine").collection("order");
         // food api for get
         app.get('/foods', async (req,res)=>{
             const query={};
             const result = await foodCollection.find(query).toArray();
             res.send(result)
+        })
+        app.get('/order', async (req,res)=>{
+            const query={};
+            const result = await orderCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        // post api for order
+
+        app.post("/order/:id", async (req,res)=>{
+            const id=req.params.id;
+            const query = {_id: ObjectId(id)}
+            const orderFood= await foodCollection.findOne(query);
+            const order= await orderCollection.insertOne(orderFood);
+
+            res.send({data:order,status:"success"});
+
         })
 
     }finally{
